8ea7440c0883829c173635ce285ca07b6e8ec539
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-07-30 13:22:46 -0400

    Delete core/metrics.py

--- core/metrics.py
+++
@@ -1,7 +0,0 @@
-def calculate_status(slo_target, current_value):
-    try:
-        target = float(slo_target.strip('%'))
-        current = float(current_value.strip('%'))
-        return "Met" if current >= target else "Not Met"
-    except:
-        return "Unknown"
