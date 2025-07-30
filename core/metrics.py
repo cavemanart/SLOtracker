644b53248f13c6f744fb6fd4d530817dccc143aa
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-07-30 12:25:52 -0400

    Delete core/metrics.py

--- core/metrics.py
+++
@@ -1,8 +0,0 @@
-def evaluate_status(target, current, reverse=False):
-    try:
-        if reverse:
-            return "Met" if current <= target else "Not Met"
-        else:
-            return "Met" if current >= target else "Not Met"
-    except Exception:
-        return "N/A"
