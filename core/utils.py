8d69126417c7e8f7e54ea4872d327e64517ad552
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-07-30 13:37:46 -0400

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def load_styles():
     with open("assets/styles.css") as f:
-        st.markdown(f\"\"\"<style>{f.read()}</style>\"\"\", unsafe_allow_html=True)
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
