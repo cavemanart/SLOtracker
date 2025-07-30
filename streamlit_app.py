1c0a26e32a8af311dddd02111613b8e8c48f5f9b
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-07-30 13:37:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,30 @@
 import streamlit as st
+from components.dashboard import render_dashboard
+from components.input_slo import render_slo_input
+from components.trends import render_trend_charts
+from components.postmortems import render_postmortem_tracker
+from components.incidents import render_incident_tracker
 from core.utils import load_styles
-from components.input_slo import show_slo_input
-from components.dashboard import show_dashboard
-from components.trends import show_trends
-from components.incidents import show_incidents
-from components.postmortems import show_postmortems
 
+st.set_page_config(page_title="SLOTracker Pro", layout="wide")
 load_styles()
 
-st.sidebar.title("SLOTracker Pro")
-tab = st.sidebar.radio("Navigate", [
-    "SLO Input",
-    "Dashboard",
-    "Trends",
-    "Incidents",
-    "Postmortems"
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", [
+    "SLO Dashboard",
+    "Add / Evaluate SLOs",
+    "Trends & History",
+    "Incident & MTTR",
+    "Postmortems",
 ])
 
-if tab == "SLO Input":
-    show_slo_input()
-elif tab == "Dashboard":
-    show_dashboard()
-elif tab == "Trends":
-    show_trends()
-elif tab == "Incidents":
-    show_incidents()
-elif tab == "Postmortems":
-    show_postmortems()
+if page == "SLO Dashboard":
+    render_dashboard()
+elif page == "Add / Evaluate SLOs":
+    render_slo_input()
+elif page == "Trends & History":
+    render_trend_charts()
+elif page == "Incident & MTTR":
+    render_incident_tracker()
+elif page == "Postmortems":
+    render_postmortem_tracker()
