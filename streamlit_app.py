619bae0c56757416bce27ddd75739053fbcd438a
cavemanart <108423418+cavemanart@users.noreply.github.com>
2025-07-31 15:33:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,20 @@
 import streamlit as st
 from components.incidents import render_incident_log
-from components.postmortem import render_postmortem_log
-from components.input_slo import render_slo_input
-from components.slo_dashboard import render_slo_dashboard
-from components.trends import render_trends
-from core.utils import load_styles
+from components.slo_dashboard import render_dashboard
+from components.input_slo import render_input_slo
+from components.postmortem import render_postmortem
 
-st.set_page_config(page_title="SLO Tracker", layout="wide")
-load_styles()
-
-st.title("📊 SLO Tracker")
-
-tabs = st.tabs(["SLO Dashboard", "Input SLOs", "Incident Log", "Postmortem Log", "Trends"])
+st.set_page_config(layout="wide")
+tabs = st.tabs(["SLO Dashboard", "Input SLOs", "Incident Log", "Postmortems"])
 
 with tabs[0]:
-    render_slo_dashboard()
+    render_dashboard()
 
 with tabs[1]:
-    render_slo_input()
+    render_input_slo()
 
 with tabs[2]:
     render_incident_log()
 
 with tabs[3]:
-    render_postmortem_log()
-
-with tabs[4]:
-    render_trends()
+    render_postmortem()
